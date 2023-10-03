5994982079a82240a174ad6c2c16b0aebfc81cd3
livotieno <livio5261@gmail.com>
2023-10-03 18:50:50 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
   
-}
+    const input = document.querySelector("#searchByID");
+    const form = document.querySelector("form");
+    const title = document.querySelector("h4");
+    const summary = document.querySelector("p");
+
+    form.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+        const idToSearch = input.value;
+
+        fetch(`http://localhost:3000/movies/${idToSearch}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            summary.textContent = data.summary;
+            title.textContent = data.title;
+        });
+    });
+};
 
 document.addEventListener('DOMContentLoaded', init);
